a63f85c4592fe1494d7d587df614cc9129937204
lichao201626 <824683639@qq.com>
2018-06-23 18:55:05 +0800

    add component for tube

--- src/component/Two.js
+++ src/component/Two.js
@@ -1,18 +1,95 @@
 import React, { Component } from "react";
 // import logo from "../../public/img/logo.svg";
 import "../App.scss";
+import * as THREE from "three";
+import Box from "grommet/components/Box";
+
+/* const scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight); */
+// document.body.appendChild(renderer.domElement);
 
 class Two extends Component {
+  constructor(props) {
+    super(props);
+
+    this.start = this.start.bind(this);
+    this.stop = this.stop.bind(this);
+    this.animate = this.animate.bind(this);
+  }
+
+  componentDidMount() {
+    console.log(window);
+    const width = this.mount.clientWidth;
+    const height = this.mount.clientHeight;
+
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000);
+    const renderer = new THREE.WebGLRenderer({ antialias: true });
+    const geometry = new THREE.BoxGeometry(1, 1, 1);
+    const material = new THREE.MeshBasicMaterial({ color: "#433F81" });
+    const cube = new THREE.Mesh(geometry, material);
+
+    camera.position.z = 4;
+    scene.add(cube);
+    const material2 = new THREE.MeshBasicMaterial({ color: "#000000" });
+    const cube2 = new THREE.Mesh(geometry, material2);
+    scene.add(cube2);
+    renderer.setClearColor("#000000");
+    renderer.setSize(width, height);
+
+    this.scene = scene;
+    this.camera = camera;
+    this.renderer = renderer;
+    this.material = material;
+    this.cube = cube;
+
+    this.mount.appendChild(this.renderer.domElement);
+    this.start();
+  }
+
+  componentWillUnmount() {
+    this.stop();
+    this.mount.removeChild(this.renderer.domElement);
+  }
+
+  start() {
+    if (!this.frameId) {
+      this.frameId = requestAnimationFrame(this.animate);
+    }
+  }
+
+  stop() {
+    cancelAnimationFrame(this.frameId);
+  }
+
+  animate() {
+    this.cube.rotation.x += 0.01;
+    this.cube.rotation.y += 0.01;
+
+    this.renderScene();
+    this.frameId = window.requestAnimationFrame(this.animate);
+  }
+
+  renderScene() {
+    this.renderer.render(this.scene, this.camera);
+  }
+  // style={{ width: "400px", height: "400px" }}
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">This is React Header</h1>
-        </header>
-        <p className="App-intro">
-          TwoTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTT
-        </p>
-      </div>
+      <div
+        style={{ width: "1800px", height: "1000px" }}
+        ref={mount => {
+          this.mount = mount;
+        }}
+      />
     );
   }
 }
