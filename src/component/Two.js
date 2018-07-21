6dd03b7af497d93827e2170e6537d23425d0c5f1
lic201626 <824683639@qq.com>
2018-07-21 16:39:42 +0800

    mouse event

--- src/component/Two.js
+++ src/component/Two.js
@@ -2,12 +2,33 @@ import React, { Component } from 'react';
 // import logo from "../../public/img/logo.svg";
 import '../App.scss';
 import * as THREE from 'three';
+const OrbitControls = require('three-orbitcontrols');
+const TrackballControls = require('three-trackballcontrols');
+
 import Box from 'grommet/components/Box';
 let meshes = require('./meshes');
 
 // document.body.appendChild(renderer.domElement);
-var camera, scene, renderer;
-var geometry, material, mesh;
+let camera, controls, scene, renderer;
+let geometry, material, mesh;
+var raycaster, mouse;
+
+let width, height;
+var intersects, intersect;
+let isRotating;
+let startPoint;
+let trackballControls;
+let movePoint;
+
+var xLine = new THREE.Vector3(1, 0, 0); //X轴正方向
+var xLineAd = new THREE.Vector3(-1, 0, 0); //X轴负方向
+var yLine = new THREE.Vector3(0, 1, 0); //Y轴正方向
+var yLineAd = new THREE.Vector3(0, -1, 0); //Y轴负方向
+var zLine = new THREE.Vector3(0, 0, 1); //Z轴正方向
+var zLineAd = new THREE.Vector3(0, 0, -1); //Z轴负方向
+
+let meshesArray;
+var normalize;
 let mesh2;
 class Two extends Component {
 	constructor(props) {
@@ -17,6 +38,18 @@ class Two extends Component {
 		this.stop = this.stop.bind(this);
 		this.animate = this.animate.bind(this);
 		this.init = this.init.bind(this);
+		this.startCube = this.startCube.bind(this);
+		this.moveCube = this.moveCube.bind(this);
+		this.stopCube = this.stopCube.bind(this);
+		this.getIntersects = this.getIntersects.bind(this);
+		this.rotateAnimation = this.rotateAnimation.bind(this);
+		this.getDirection = this.getDirection.bind(this);
+		this.min = this.min.bind(this);
+		this.getBoxs = this.getBoxs.bind(this);
+		this.updateCubeIndex = this.updateCubeIndex.bind(this);
+		this.rotateAroundWorldX = this.rotateAroundWorldX.bind(this);
+		this.rotateAroundWorldY = this.rotateAroundWorldY.bind(this);
+		this.rotateAroundWorldZ = this.rotateAroundWorldZ.bind(this);
 	}
 
 	componentDidMount() {
@@ -26,7 +59,7 @@ class Two extends Component {
         this.camera = camera;
         this.renderer = renderer;
         this.material = material; */
-		// this.cube = cubes;
+		// this.cube = meshesArray;
 
 		// this.mount.appendChild(this.renderer.domElement);
 		// this.renderer.render(this.scene, this.camera);
@@ -41,17 +74,26 @@ class Two extends Component {
 	}
 
 	init() {
+		width = window.innerWidth;
+		height = window.innerHeight;
+
+		raycaster = new THREE.Raycaster();
+		mouse = new THREE.Vector2();
+
 		camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.03, 10);
+		//controls = new OrbitControls(camera);
 		camera.position.x = 0.4;
 		camera.position.y = 0.4;
 		camera.position.z = 1.4;
 		// camera.lookAt(scene.position);
+		//console.log('cccc', controls);
+		// controls.update();
 
 		scene = new THREE.Scene();
 		var light = new THREE.PointLight(0x8844ff, 5, 100);
 		scene.add(light);
 
-		let meshesArray = meshes.create27Meshes(0.1);
+		meshesArray = meshes.create27Meshes(0.1);
 		console.log(meshesArray);
 		meshesArray.forEach((element) => {
 			var box = new THREE.BoxHelper(element);
@@ -65,17 +107,414 @@ class Two extends Component {
 		renderer.setSize(window.innerWidth, window.innerHeight);
 		renderer.setClearColor(0xffffff, 1.0);
 		document.body.appendChild(renderer.domElement);
+
+		trackballControls = new TrackballControls(camera);
+		trackballControls.rotateSpeed = 1;
+		trackballControls.panSpeed = 0;
+		var clock = new THREE.Clock();
+		var delta = clock.getDelta();
+		trackballControls.update(delta);
+
+		renderer.domElement.addEventListener('mousedown', this.startCube, false);
+		renderer.domElement.addEventListener('mousemove', this.moveCube, false);
+		renderer.domElement.addEventListener('mouseup', this.stopCube, false);
+
+		renderer.domElement.addEventListener('touchstart', this.startCube, false);
+		renderer.domElement.addEventListener('touchmove', this.moveCube, false);
+		renderer.domElement.addEventListener('touchend', this.stopCube, false);
 	}
+
+	onMouseMove(event) {
+		mouse.x = event.clientX / window.innerWidth * 2 - 1;
+		mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+	}
+
+	//更新位置索引
+	updateCubeIndex(elements) {
+		for (var i = 0; i < elements.length; i++) {
+			var temp1 = elements[i];
+			for (var j = 0; j < beginStatus.length; j++) {
+				var temp2 = beginStatus[j];
+				if (
+					Math.abs(temp1.position.x - temp2.x) <= 2 &&
+					Math.abs(temp1.position.y - temp2.y) <= 2 &&
+					Math.abs(temp1.position.z - temp2.z) <= 2
+				) {
+					temp1.cubeIndex = temp2.cubeIndex;
+					break;
+				}
+			}
+		}
+	}
+	//绕着世界坐标系的某个轴旋转
+	rotateAroundWorldY(obj, rad) {
+		var x0 = obj.position.x;
+		var z0 = obj.position.z;
+		var q = new THREE.Quaternion();
+		q.setFromAxisAngle(new THREE.Vector3(0, 1, 0), rad);
+		obj.quaternion.premultiply(q);
+		//obj.rotateY(rad);
+		obj.position.x = Math.cos(rad) * x0 + Math.sin(rad) * z0;
+		obj.position.z = Math.cos(rad) * z0 - Math.sin(rad) * x0;
+	}
+	rotateAroundWorldZ(obj, rad) {
+		var x0 = obj.position.x;
+		var y0 = obj.position.y;
+		var q = new THREE.Quaternion();
+		q.setFromAxisAngle(new THREE.Vector3(0, 0, 1), rad);
+		obj.quaternion.premultiply(q);
+		//obj.rotateZ(rad);
+		obj.position.x = Math.cos(rad) * x0 - Math.sin(rad) * y0;
+		obj.position.y = Math.cos(rad) * y0 + Math.sin(rad) * x0;
+	}
+	rotateAroundWorldX(obj, rad) {
+		var y0 = obj.position.y;
+		var z0 = obj.position.z;
+		var q = new THREE.Quaternion();
+		q.setFromAxisAngle(new THREE.Vector3(1, 0, 0), rad);
+		obj.quaternion.premultiply(q);
+		//obj.rotateX(rad);
+		obj.position.y = Math.cos(rad) * y0 - Math.sin(rad) * z0;
+		obj.position.z = Math.cos(rad) * z0 + Math.sin(rad) * y0;
+	}
+
+	getIntersects(event) {
+		// console.log('get Intersects event', event);
+		if (event.touches) {
+			var touch = event.touches[0];
+			mouse.x = touch.clientX / width * 2 - 1;
+			mouse.y = -(touch.clientY / height) * 2 + 1;
+		} else {
+			mouse.x = event.clientX / width * 2 - 1;
+			mouse.y = -(event.clientY / height) * 2 + 1;
+		}
+		// console.log(raycaster, 'RRRRRRRRRRRRRRRRRRRRRRR');
+		raycaster.setFromCamera(mouse, camera);
+		// console.log(raycaster, 'RRRRRRRRRRRRRRRRRRRRRRR2');
+		// console.log(scene.children, 'sceneCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCc');
+		intersects = raycaster.intersectObjects(scene.children);
+		// console.log(intersects, '############');
+		if (intersects.length) {
+			try {
+				if (intersects[0].object.cubeType === 'coverCube') {
+					intersect = intersects[1];
+					// normalize = intersects[0];
+					normalize = new THREE.Vector3(0, 1, 0);
+				} else {
+					intersect = intersects[0];
+					// normalize = intersects[1];
+					normalize = new THREE.Vector3(0, 1, 0);
+				}
+			} catch (err) {
+				//nothing
+			}
+		}
+	}
+	//根据方向获得运动元素
+	getBoxs(target, direction) {
+		var targetId = target.object.cubeIndex;
+		var ids = [];
+		for (var i = 0; i < meshesArray.length; i++) {
+			ids.push(meshesArray[i].cubeIndex);
+		}
+		var minId = this.min(ids);
+		targetId = targetId - minId;
+		var numI = parseInt(targetId / 9);
+		var numJ = targetId % 9;
+		var boxs = [];
+		//根据绘制时的规律判断 no = i*9+j
+		switch (direction) {
+			//绕z轴
+			case 0.1:
+			case 0.2:
+			case 1.1:
+			case 1.2:
+			case 2.3:
+			case 2.4:
+			case 3.3:
+			case 3.4:
+				for (var i = 0; i < meshesArray.length; i++) {
+					var tempId = meshesArray[i].cubeIndex - minId;
+					if (numI === parseInt(tempId / 9)) {
+						boxs.push(meshesArray[i]);
+					}
+				}
+				break;
+			//绕y轴
+			case 0.3:
+			case 0.4:
+			case 1.3:
+			case 1.4:
+			case 4.3:
+			case 4.4:
+			case 5.3:
+			case 5.4:
+				for (var i = 0; i < meshesArray.length; i++) {
+					var tempId = meshesArray[i].cubeIndex - minId;
+					if (parseInt(numJ / 3) === parseInt((tempId % 9) / 3)) {
+						boxs.push(meshesArray[i]);
+					}
+				}
+				break;
+			//绕x轴
+			case 2.1:
+			case 2.2:
+			case 3.1:
+			case 3.2:
+			case 4.1:
+			case 4.2:
+			case 5.1:
+			case 5.2:
+				for (var i = 0; i < meshesArray.length; i++) {
+					var tempId = meshesArray[i].cubeIndex - minId;
+					if ((tempId % 9) % 3 === numJ % 3) {
+						boxs.push(meshesArray[i]);
+					}
+				}
+				break;
+			default:
+				break;
+		}
+		return boxs;
+	}
+	//获取数组中的最小值
+	min(arr) {
+		var min = arr[0];
+		for (var i = 1; i < arr.length; i++) {
+			if (arr[i] < min) {
+				min = arr[i];
+			}
+		}
+		return min;
+	}
+	//获得旋转方向
+	getDirection(vector3) {
+		console.log('getDriection');
+		var direction;
+		//判断差向量和x、y、z轴的夹角
+		var xAngle = vector3.angleTo(xLine);
+		var xAngleAd = vector3.angleTo(xLineAd);
+		var yAngle = vector3.angleTo(yLine);
+		var yAngleAd = vector3.angleTo(yLineAd);
+		var zAngle = vector3.angleTo(zLine);
+		var zAngleAd = vector3.angleTo(zLineAd);
+		var minAngle = this.min([ xAngle, xAngleAd, yAngle, yAngleAd, zAngle, zAngleAd ]); //最小夹角
+
+		switch (minAngle) {
+			case xAngle:
+				direction = 0; //向x轴正方向旋转90度（还要区分是绕z轴还是绕y轴）
+				if (normalize.equals(yLine)) {
+					direction = direction + 0.1; //绕z轴顺时针
+				} else if (normalize.equals(yLineAd)) {
+					direction = direction + 0.2; //绕z轴逆时针
+				} else if (normalize.equals(zLine)) {
+					direction = direction + 0.3; //绕y轴逆时针
+				} else {
+					direction = direction + 0.4; //绕y轴顺时针
+				}
+				break;
+			case xAngleAd:
+				direction = 1; //向x轴反方向旋转90度
+				if (normalize.equals(yLine)) {
+					direction = direction + 0.1; //绕z轴逆时针
+				} else if (normalize.equals(yLineAd)) {
+					direction = direction + 0.2; //绕z轴顺时针
+				} else if (normalize.equals(zLine)) {
+					direction = direction + 0.3; //绕y轴顺时针
+				} else {
+					direction = direction + 0.4; //绕y轴逆时针
+				}
+				break;
+			case yAngle:
+				direction = 2; //向y轴正方向旋转90度
+				if (normalize.equals(zLine)) {
+					direction = direction + 0.1; //绕x轴逆时针
+				} else if (normalize.equals(zLineAd)) {
+					direction = direction + 0.2; //绕x轴顺时针
+				} else if (normalize.equals(xLine)) {
+					direction = direction + 0.3; //绕z轴逆时针
+				} else {
+					direction = direction + 0.4; //绕z轴顺时针
+				}
+				break;
+			case yAngleAd:
+				direction = 3; //向y轴反方向旋转90度
+				if (normalize.equals(zLine)) {
+					direction = direction + 0.1; //绕x轴顺时针
+				} else if (normalize.equals(zLineAd)) {
+					direction = direction + 0.2; //绕x轴逆时针
+				} else if (normalize.equals(xLine)) {
+					direction = direction + 0.3; //绕z轴顺时针
+				} else {
+					direction = direction + 0.4; //绕z轴逆时针
+				}
+				break;
+			case zAngle:
+				direction = 4; //向z轴正方向旋转90度
+				if (normalize.equals(yLine)) {
+					direction = direction + 0.1; //绕x轴顺时针
+				} else if (normalize.equals(yLineAd)) {
+					direction = direction + 0.2; //绕x轴逆时针
+				} else if (normalize.equals(xLine)) {
+					direction = direction + 0.3; //绕y轴顺时针
+				} else {
+					direction = direction + 0.4; //绕y轴逆时针
+				}
+				break;
+			case zAngleAd:
+				direction = 5; //向z轴反方向旋转90度
+				if (normalize.equals(yLine)) {
+					direction = direction + 0.1; //绕x轴逆时针
+				} else if (normalize.equals(yLineAd)) {
+					direction = direction + 0.2; //绕x轴顺时针
+				} else if (normalize.equals(xLine)) {
+					direction = direction + 0.3; //绕y轴逆时针
+				} else {
+					direction = direction + 0.4; //绕y轴顺时针
+				}
+				break;
+			default:
+				break;
+		}
+		return direction;
+	}
+
+	startCube(event) {
+		console.log('mouse down', event);
+		this.getIntersects(event);
+		if (!isRotating && intersect) {
+			startPoint = intersect.point;
+			trackballControls.enabled = false;
+		} else {
+			trackballControls.enabled = true;
+		}
+	}
+
+	stopCube() {
+		console.log('mouse up');
+		intersect = null;
+		startPoint = null;
+	}
+
+	moveCube(event) {
+		console.log('mouse move', event);
+		this.getIntersects(event);
+		// console.log('mouseMove', intersect);
+		if (intersect) {
+			if (!isRotating && startPoint) {
+				movePoint = intersect.point;
+				console.log(movePoint, 'MPMPMMMMMMMMMMMMMMMMM');
+				console.log(startPoint, 'SPSPSPSSSSSSSSSSSSSSSSSSSSS');
+				if (!movePoint.equals(startPoint)) {
+					console.log(startPoint, '【【【【【【【【【【【【【【【【【【【【【【【【');
+					isRotating = true;
+					var sub = movePoint.sub(startPoint);
+					var direction = this.getDirection(sub);
+					console.log(startPoint, '））））））））））））））））', direction);
+					var elements = this.getBoxs(intersect, direction);
+					var startTime = new Date().getTime();
+					console.log('before request anim');
+					var that = this;
+					requestAnimationFrame(function(timestamp) {
+						console.log('callback anim');
+						that.rotateAnimation(elements, direction, timestamp, 0);
+					});
+				}
+			}
+		}
+		event.preventDefault();
+	}
+
+	rotateAnimation(elements, direction, currentstamp, startstamp, laststamp) {
+		console.log('rotateAnimation');
+		var totalTime = 500;
+		if (startstamp === 0) {
+			startstamp = currentstamp;
+			laststamp = currentstamp;
+		}
+		if (currentstamp - startstamp >= totalTime) {
+			currentstamp = startstamp + totalTime;
+			isRotating = false;
+			startPoint = null;
+			this.updateCubeIndex(elements);
+		}
+		switch (direction) {
+			//绕z轴顺时针
+			case 0.1:
+			case 1.2:
+			case 2.4:
+			case 3.3:
+				for (var i = 0; i < elements.length; i++) {
+					this.rotateAroundWorldZ(elements[i], -90 * Math.PI / 180 * (currentstamp - laststamp) / totalTime);
+				}
+				break;
+			//绕z轴逆时针
+			case 0.2:
+			case 1.1:
+			case 2.3:
+			case 3.4:
+				for (var i = 0; i < elements.length; i++) {
+					this.rotateAroundWorldZ(elements[i], 90 * Math.PI / 180 * (currentstamp - laststamp) / totalTime);
+				}
+				break;
+			//绕y轴顺时针
+			case 0.4:
+			case 1.3:
+			case 4.3:
+			case 5.4:
+				for (var i = 0; i < elements.length; i++) {
+					this.rotateAroundWorldY(elements[i], -90 * Math.PI / 180 * (currentstamp - laststamp) / totalTime);
+				}
+				break;
+			//绕y轴逆时针
+			case 1.4:
+			case 0.3:
+			case 4.4:
+			case 5.3:
+				for (var i = 0; i < elements.length; i++) {
+					this.rotateAroundWorldY(elements[i], 90 * Math.PI / 180 * (currentstamp - laststamp) / totalTime);
+				}
+				break;
+			//绕x轴顺时针
+			case 2.2:
+			case 3.1:
+			case 4.1:
+			case 5.2:
+				for (var i = 0; i < elements.length; i++) {
+					this.rotateAroundWorldX(elements[i], 90 * Math.PI / 180 * (currentstamp - laststamp) / totalTime);
+				}
+				break;
+			//绕x轴逆时针
+			case 2.1:
+			case 3.2:
+			case 4.2:
+			case 5.1:
+				for (var i = 0; i < elements.length; i++) {
+					this.rotateAroundWorldX(elements[i], -90 * Math.PI / 180 * (currentstamp - laststamp) / totalTime);
+				}
+				break;
+			default:
+				break;
+		}
+		var _that = this;
+		if (currentstamp - startstamp < totalTime) {
+			requestAnimationFrame(function(timestamp) {
+				_that.rotateAnimation(elements, direction, timestamp, startstamp, currentstamp);
+			});
+		}
+	}
+
+	render() {}
 
 	animate() {
 		requestAnimationFrame(this.animate);
-		scene.children.forEach((child) => {
+		/* 		scene.children.forEach((child) => {
 			if (child.type == 'Mesh') {
 				// console.log(child);
 				// child.rotateX(Math.PI / 2);
 				child.rotation.x += 0.01;
 			}
-		});
+		}); */
 		// mesh.rotateX(Math.PI / 2);
 		// mesh.rotation.x += 0.01;
 		// mesh.rotation.y += 0.02;
@@ -83,6 +522,7 @@ class Two extends Component {
 		// mesh2.rotation.x += 0.01;
 		// mesh2.rotation.y += 0.02;
 		// camera.position.y -= 0.002;
+		// controls.update();
 		renderer.render(scene, camera);
 	}
 	start() {
